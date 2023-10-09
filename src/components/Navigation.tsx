97d5225671691c803c3bef1c2ca40155f3a84fcc
danny <danny.nothdurft@icloud.com>
2023-10-09 15:36:32 +0200

    Navi debug

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,36 +1,51 @@
-import React, { useState } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 import { RiMenuLine } from "@react-icons/all-files/ri/RiMenuLine";
 import { AiOutlineClose } from "@react-icons/all-files/ai/AiOutlineClose";
 
-import Logo from '@/images/Logo.png';
+import Logo from "@/images/Logo.png";
 
 function Navigation() {
-
-    const [aktiv, setAktiv] = useState<boolean>(false)
-
-    const toggle = () => {
-        setAktiv(!aktiv);
-        console.log(aktiv)
+  const [aktiv, setAktiv] = useState<boolean>(false);
+  const toggle = () => {
+    if (window.innerWidth <= 520) {
+      setAktiv(!aktiv);
+      console.log(aktiv);
     }
+  };
 
-    return (
-        <div className='Navigation-Componente'>
-            <Link href="/">
-                <Image src={Logo} width={50} height={50} alt="Danny Nothdurft Logo" className='DN-Logo' />
-            </Link>
-            <ul onClick={toggle} className={aktiv ? "aktive" : ""}>
-                <li> <Link href="/">Home</Link> </li>
-                <li> <Link href="/projects">Projekte</Link> </li>
-                <li> <Link href="/contact">Kontakt</Link> </li>
-            </ul>
+  return (
+    <div className="Navigation-Componente">
+      <Link href="/">
+        <Image
+          src={Logo}
+          width={50}
+          height={50}
+          alt="Danny Nothdurft Logo"
+          className="DN-Logo"
+        />
+      </Link>
+      <ul onClick={toggle} className={aktiv ? "aktive" : ""}>
+        <li>
+          {" "}
+          <Link href="/">Home</Link>{" "}
+        </li>
+        <li>
+          {" "}
+          <Link href="/projects">Projekte</Link>{" "}
+        </li>
+        <li>
+          {" "}
+          <Link href="/contact">Kontakt</Link>{" "}
+        </li>
+      </ul>
 
-            <button onClick={toggle} className="toggle">
-                {aktiv ? <AiOutlineClose /> : <RiMenuLine />}
-            </button>
-        </div>
-    )
+      <button onClick={toggle} className="toggle">
+        {aktiv ? <AiOutlineClose /> : <RiMenuLine />}
+      </button>
+    </div>
+  );
 }
 
 export default Navigation;
