778a4e15608479edcdb2f9a05de4ffe48bcf7f74
danny <danny.nothdurft@icloud.com>
2024-04-26 19:48:00 +0200

    debug Deplay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,7 @@ export default function RootLayout({
     <html lang="de">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
           rel="stylesheet"
