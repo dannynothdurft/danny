2ff22223d84503763728868b974c851dbb8a00c3
danny <danny.nothdurft@icloud.com>
2024-04-28 11:40:04 +0200

    SEO optimierung

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -11,18 +11,18 @@ const AboutMe = () => {
       <h2 className="section__title">{me.titel}</h2>
 
       <p className="text">
-        Hallo, mein Name ist Danny, ich bin 32 Jahre alt und stamme aus der
-        wunderschönen Stadt Hamburg. Mein beruflicher Werdegang war eine
-        spannende Reise, auf der ich verschiedene Pfade erkundet habe, um
+        Hallo, mein Name ist <b>Danny</b>, ich bin 32 Jahre alt und stamme aus
+        der <b>wunderschönen Stadt Hamburg</b>. Mein beruflicher Werdegang war
+        eine spannende Reise, auf der ich verschiedene Pfade erkundet habe, um
         herauszufinden, wo meine Leidenschaft und mein Talent wirklich liegen.
-        Nachdem ich meine Ausbildung als Kfz-Mechatroniker begonnen hatte,
-        sammelte ich über sechs Jahre lang wertvolle Erfahrungen in drei
+        Nachdem ich meine <b>Ausbildung als Kfz-Mechatroniker begonnen</b>{" "}
+        hatte, sammelte ich über sechs Jahre lang wertvolle Erfahrungen in drei
         unterschiedlichen Unternehmen. Diese Zeit ermöglichte es mir, ein
         breites Spektrum an Fähigkeiten und Kenntnissen im Bereich der
         Fahrzeugmechanik zu entwickeln und zu vertiefen.
       </p>
       <p className="text">
-        Auf meiner beruflichen Entdeckungsreise habe ich auch andere Wege
+        Auf meiner <b>beruflichen Entdeckungsreise</b> habe ich auch andere Wege
         erkundet. Für etwas mehr als ein Jahr war ich als Kurier tätig, wodurch
         ich meine organisatorischen Fähigkeiten und meine Fähigkeit, unter
         Zeitdruck effizient zu arbeiten, weiterentwickelt habe. Anschließend
@@ -38,7 +38,8 @@ const AboutMe = () => {
         beruflichen Herausforderungen zu suchen.
       </p>
       <p className="text">
-        Im Jahr 2020 bot sich mir dann die aufregende Möglichkeit, bei Rafi
+        Im Jahr 2020 bot sich mir dann die aufregende Möglichkeit, bei{" "}
+        <b>Rafi</b>
         einzusteigen und beim Aufbau eines neuen Unternehmens mitzuwirken. Diese
         Erfahrung war für mich von unschätzbarem Wert, da ich die Gelegenheit
         hatte, in verschiedene Bereiche einzutauchen und meine Fähigkeiten
@@ -55,15 +56,15 @@ const AboutMe = () => {
         sicherzustellen.
       </p>
       <p className="text">
-        Im Jahr 2021 gründete ich Moin Moin Möbelmonteur und begann, eine
+        Im Jahr 2021 gründete ich <b>Moin Moin Möbelmonteur</b> und begann, eine
         Vielzahl von Kunden zu betreuen. Alles lief gut, bis die
-        COVID-19-Pandemie zuschlug und die wirtschaftliche Landschaft
+        <b>COVID-19-Pandemie</b> zuschlug und die wirtschaftliche Landschaft
         veränderte. Leider zwang mich die finanzielle Belastung der Pandemie
         dazu, Insolvenz anzumelden, da meine Ausgaben die Einnahmen überstiegen.
         Doch wie sagt man so schön, wenn sich eine Tür schließt, öffnet sich
         eine andere. In dieser schwierigen Zeit entschied ich mich, eine neue
         berufliche Richtung einzuschlagen und absolvierte eine Umschulung bei
-        DCI zum Web- und Softwareentwickler.
+        <b>DCI zum Web- und Softwareentwickler</b>.
       </p>
       <p className="text">
         Während meiner Zeit bei DCI tauchte ich in die Welt der Entwicklung ein
@@ -85,7 +86,7 @@ const AboutMe = () => {
         war.
       </p>
       <p className="text">
-        Im Jahr 2022 begann ich ein Praktikum bei SugarShape, einem
+        Im Jahr 2022 begann ich ein Praktikum bei <b>SugarShape</b>, einem
         aufstrebenden Unternehmen, das als Unterwäsche-Label bekannt ist. Nach
         meinem Praktikum boten sie mir eine Stelle als Junior Fullstack
         Developer an. Seitdem arbeite ich an einer Vielzahl spannender
@@ -94,37 +95,38 @@ const AboutMe = () => {
         für verschiedene Kampagnen und Events.
       </p>
       <p className="text">
-        Im Jahr 2024 starteten wir bei SugarShape mit dem Umzug unseres
+        Im Jahr 2024 starteten wir bei <b>SugarShape</b> mit dem Umzug unseres
         Oxid-Shops zu Shopify, was eine aufregende neue Phase in meiner Karriere
         als Entwickler darstellte.
       </p>
       <p className="text">
-        Neben meiner Tätigkeit bei SugarShape investiere ich auch Zeit und
-        Energie in private Projekte, mit denen ich mein Portfolio erweitern und
-        mir langfristig ein zusätzliches Einkommen erhoffe. Bislang habe ich
+        Neben meiner Tätigkeit bei <p>SugarShape</p> investiere ich auch Zeit
+        und Energie in private Projekte, mit denen ich mein Portfolio erweitern
+        und mir langfristig ein zusätzliches Einkommen erhoffe. Bislang habe ich
         bereits vier vielversprechende Projekte in der Pipeline.
       </p>
       <p className="text">
-        Diese privaten Projekte bieten mir nicht nur die Möglichkeit, mein
-        Fachwissen und meine Fähigkeiten weiterzuentwickeln, sondern auch, meine
-        kreative Seite auszuleben und neue Technologien zu erkunden. Jedes
-        Projekt birgt seine eigenen Herausforderungen und Chancen, und ich bin
-        bestrebt, sie alle erfolgreich umzusetzen.
+        Diese <a href="#projects">privaten Projekte</a> bieten mir nicht nur die
+        Möglichkeit, mein Fachwissen und meine Fähigkeiten weiterzuentwickeln,
+        sondern auch, meine kreative Seite auszuleben und neue Technologien zu
+        erkunden. Jedes Projekt birgt seine eigenen Herausforderungen und
+        Chancen, und ich bin bestrebt, sie alle erfolgreich umzusetzen.
       </p>
       <p className="text">
-        Die Vielfalt meiner Projekte spiegelt meine breite Interessenpalette
-        wider und ermöglicht es mir, mich in verschiedenen Bereichen der
-        Softwareentwicklung zu engagieren. Von der Entwicklung innovativer Apps
-        bis hin zur Gestaltung benutzerfreundlicher Websites - ich bin stets
-        bestrebt, meine Fähigkeiten zu erweitern und neue Wege zu finden, um
-        Mehrwert zu schaffen.
+        Die Vielfalt meiner <a href="#projects">Projekte</a> spiegelt meine
+        breite Interessenpalette wider und ermöglicht es mir, mich in
+        verschiedenen Bereichen der Softwareentwicklung zu engagieren. Von der
+        Entwicklung innovativer Apps bis hin zur Gestaltung benutzerfreundlicher
+        Websites - ich bin stets bestrebt, meine Fähigkeiten zu erweitern und
+        neue Wege zu finden, um Mehrwert zu schaffen.
       </p>
       <p className="text">
-        Durch meine privaten Projekte kann ich meine Kreativität und
-        Innovationskraft voll entfalten und gleichzeitig mein unternehmerisches
-        Denken schärfen. Mein Ziel ist es, diese Projekte langfristig
-        erfolgreich zu etablieren und mein eigenes Unternehmen aufzubauen, das
-        auf meinen individuellen Fähigkeiten und Leidenschaften basiert.
+        Durch meine <a href="#projects">privaten Projekte</a> kann ich meine
+        Kreativität und Innovationskraft voll entfalten und gleichzeitig mein
+        unternehmerisches Denken schärfen. Mein Ziel ist es, diese Projekte
+        langfristig erfolgreich zu etablieren und mein eigenes Unternehmen
+        aufzubauen, das auf meinen individuellen Fähigkeiten und Leidenschaften
+        basiert.
       </p>
     </div>
   );
