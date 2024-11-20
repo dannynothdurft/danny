13a6576509f2799338edd0ff5439049a2160d180
dannynothdurft <danny.nothdurft@icloud.com>
2024-11-20 18:25:40 +0100

    DSGVO for Google AdSense

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,18 @@
+import Link from "next/link";
+
 const Footer = () => (
   <footer className="footer">
-    <a href="#" target="_blank" rel="noreferrer" className="link footer__link">
+    <Link
+      href="#"
+      target="_blank"
+      rel="noreferrer"
+      className="link footer__link"
+    >
       Created by Danny Nothdurft
-    </a>
+    </Link>
+    <Link href="/dsgvo" rel="noreferrer" className="link footer__link">
+      Datenschutz
+    </Link>
   </footer>
 );
 
